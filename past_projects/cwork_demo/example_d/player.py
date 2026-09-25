c9c2e44a442811a06c833e38c45b1e10394845d4
bakerpdgit <66876832+bakerpdgit@users.noreply.github.com>
2026-09-25 21:07:42 +0100

    removed past projects

--- past_projects/cwork_demo/example_d/player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self._Name = name
-
-    def getName(self):
-        return self._Name
-
-    def setName(self, name):
-        self._Name = name
